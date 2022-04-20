c9423b6ba0e367c1f35ce451f6852e876e7e5457
Vitor_Alfredo <vitor.dragon1411@gmail.com>
2022-04-19 21:06:09 -0300

    Update locadora_aula.sql

--- locadora_aula.sql
+++ locadora_aula.sql
@@ -34,3 +34,51 @@ insert into categoria value ('0', 'Suspense');
 insert into categoria value ('0', 'Ficção');
 insert into categoria value ('0', 'Comédia');
 insert into categoria value ('0', 'Romance');
+
+create table filme_texto(
+	filme_id int not null auto_increment,
+    titulo varchar(255) not null,
+	primary key (filme_id)
+);
+
+select * from filme_texto;
+
+insert into filme_texto value ('0', 'O auto da compadecida');
+insert into filme_texto value ('0', 'Os Vingadores');
+insert into filme_texto value ('0', 'Doutor Estranho');
+insert into filme_texto value ('0', 'Tropa de Elite');
+insert into filme_texto value ('0', 'Até Que Sorte Nos Separe');
+insert into filme_texto value ('0', 'Aquaman');
+insert into filme_texto value ('0', 'Velozes e Furiosos');
+
+create table pais(
+	pais_id int not null auto_increment,
+    pais varchar(50) not null,
+	primary key (pais_id)
+);
+
+select * from pais;
+
+insert into pais value ('0', 'Brasil');
+insert into pais value ('0', 'Estados Unidos');
+insert into pais value ('0', 'Alemanha');
+insert into pais value ('0', 'Inglaterra');
+insert into pais value ('0', 'Canadá');
+insert into pais value ('0', 'Japão');
+insert into pais value ('0', 'Noruega');
+
+create table idioma(
+	idioma_id int not null auto_increment,
+    nome char(20) not null,
+	primary key (idioma_id)
+);
+
+select * from idioma;
+
+insert into idioma value ('0', 'Português-BR');
+insert into idioma value ('0', 'Inglês');
+insert into idioma value ('0', 'Alemão');
+insert into idioma value ('0', 'Francês');
+insert into idioma value ('0', 'Japonês');
+insert into idioma value ('0', 'Norueguês');
+
